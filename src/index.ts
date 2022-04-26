1aa67acf309e1e466e22caceedc793284a4bfcdf
Febzey <braydent1235@gmail.com>
2022-04-26 02:11:08 -0500

    w

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,33 @@ import * as Conf      from "./config.js";
 export * as endpoints from "./structure/endpoints/endpoints.js";
 export * as logger    from "./functions/utils/logger.js"
 
-export const { colors, config } = Conf;
-export const options  = new Conf.default();
-export const client   = new Client(options.discord); 
-export const bot      = new Bot(options.mineflayer);
+/**
+ * Main config and colors .json files.
+ */
+const { colors, config } = Conf;
+
+/**
+ * @class Options
+ * Main options for various functions.
+ */
+const options = new Conf.default();
+
+/**
+ * @class Client
+ * Discord bot class
+ */
+const client = new Client(options.discord); 
+
+/**
+ * @class Bot
+ * Mineflayer bot class
+ */
+const bot = new Bot(options.mineflayer);
+
+export { 
+    colors, 
+    config, 
+    options, 
+    client, 
+    bot 
+};
