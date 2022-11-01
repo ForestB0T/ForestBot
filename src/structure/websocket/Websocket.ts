447e950364b6c5370e47dbfa4fdb7a2276fd00b9
Febzey <braydent1235@gmail.com>
2022-10-31 19:52:12 -0500

    w

--- src/structure/websocket/Websocket.ts
+++ src/structure/websocket/Websocket.ts
@@ -32,42 +32,4 @@ export default class Websocket {
             }
         })
     }
-
-
 }
-
-    // webSocketConnect() {
-    //     const _wss = new WebSocket('ws://localhost:5000/ws-auth/newtest');
-    //     return this.wss = _wss;
-    // }
-
-    // sendChatMessage(user: string, msg: string, mc_server: string) {
-    //     console.log(msg)
-    //     if (this.wss.readyState !== WebSocket.OPEN) return; 
-    //     const messageToSend = JSON.stringify({ 
-    //         username: user,
-    //         message: msg,
-    //         mc_server: mc_server
-    //     })
-    //     this.wss.send(messageToSend)
-    // }
-
-    // handleEvents() {
-    //     this.wss.on('error', (err) => {
-    //         console.log(err)
-    //     })
-
-    //     this.wss.on('close', () => {
-    //         console.log("The websocket has closed")
-    //     }) 
-
-    //     this.wss.on('open', () => { 
-    //         logger.log("> Connected to websocket successfully", "blue", true)
-    //         this.wss.send(JSON.stringify({
-    //             username: "Test2",
-    //             message: "Hello this is a message",
-    //             mc_server: "newtest"
-    //         }))
-    //     })
-    // }
-
