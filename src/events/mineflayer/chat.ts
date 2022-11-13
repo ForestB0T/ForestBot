90ef889fdee1a3d2d7e586eeffbfab266d66d64a
Febzey <braydent1235@gmail.com>
2022-11-13 16:47:21 -0600

    some sloppy fixes, hopefully will work idek anymor

--- src/events/mineflayer/chat.ts
+++ src/events/mineflayer/chat.ts
@@ -1,19 +1,19 @@
 import type { BotEvents } from 'mineflayer';
 import { config, client } from "../../index.js";
-import Bot                from '../../structure/mineflayer/Bot.js';
+import Bot from '../../structure/mineflayer/Bot.js';
 
 const prefix = config.prefix;
 const spam: Map<string, number> = new Map();
 
 function antiSpamHandler(args: antiSpamArgsType): boolean {
-    const { user, Bot, cooldown_time, spam_limit } = args; 
+    const { user, Bot, cooldown_time, spam_limit } = args;
 
     if (!spam.has(user)) spam.set(user, 1);
     else spam.set(user, spam.get(user) + 1);
 
     const sUser = spam.get(user);
 
-    if (sUser === 2) { 
+    if (sUser === 2) {
         Bot.bot.whisper(user, `[Anti-Spam] - Please wait ${cooldown_time / 1000} seconds.`);
         return false;
     }
@@ -23,22 +23,22 @@ function antiSpamHandler(args: antiSpamArgsType): boolean {
         Bot.userBlacklist.add(user);
         return false
     }
-    
+
     if (sUser === 1) {
         setTimeout(() => spam.delete(user), cooldown_time)
     }
 
     return sUser <= 1 ? true : false;
-    
+
 }
 
 export default {
     name: "chat:chat",
     once: false,
     run: async (args: any[], Bot: Bot) => {
-        
+
         const content: BotEvents = args[0];
-        
+
         try {
 
             const user = {
@@ -46,43 +46,48 @@ export default {
                 message: content[0][1]
             }
 
-            if (user.message.endsWith("[w]")) { 
+            if (
+                user.message.endsWith("[w]") ||
+                user.username === "From" ||
+                user.username === "To" ||
+                user.message.split(" ")[1] === "»"
+            ) {
                 return;
             }
-    
+
             Bot.endpoints.saveChat(
                 user.username,
                 user.message,
                 Bot.mc_server
             )
-    
+
             client.chatEmbed(`**${user.username}** » ${user.message}`, "gray");
-    
+
             if (!user.message.startsWith(prefix)) return;
             for (const [key, value] of Bot.commands) {
                 for (const alias of value.commands) {
                     if (user.message.toLowerCase().startsWith(`${prefix}${alias}`)) {
                         if (Bot.userBlacklist.has(user.username)) return;
                         if (user.username === Bot.bot.username) return;
-    
+
                         if (Bot.disabledCommands.has(key) || (Bot.useWhitelist && (Bot.whitelistedCmds.has(alias) && !Bot.userWhitelist.has(user.username)))) return;
                         if (!Bot.useCommands) return;
-    
+
                         let args = user.message.split(" ")
                         args.shift();
-    
+
                         if (antiSpamHandler({
                             user: user.username,
                             Bot: Bot,
                             cooldown_time: config.anti_spam_cooldown,
                             spam_limit: config.anti_spam_msg_limit
                         })) value.execute(user.username, args, Bot);
-                    
+
                         return;
                     }
                 }
             }
-    
+
         } catch (err) {
             return console.log(err.message);
         }
