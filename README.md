0edf8646ac96a5e5e859923f30f65b5c5a68b2ac
Febzey <braydent1235@gmail.com>
2022-11-13 20:41:38 -0600

    w

--- README.md
+++ README.md
@@ -9,18 +9,3 @@ Multiple config options available.
 Records almost every statistic you would want within a Minecraft server.
 
 https://forestbot.org
-
-###### How to use.
-
-To use this bot for your self there are a few prequestions.
-1. Basic to advanced knowledge of Javascript/Typescript.
-2. Know the basics of MySql, You will need to setup tables.
-3. Read through the code to ensure nothing malicous is being done.
-4. Once the previous 3 steps are met, continue with the rest.
-5. git clone the repositroy or download from source.
-6. `CD` into the directory.
-7. Have node.js >= 16.10.0 installed.
-8. `npm install -g yarn`
-9. `yarn install`
-10. Create a .env file with the bots credentials.
-11. `yarn start`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import Bot from "./structure/mineflayer/Bot.js";
-import Client from "./structure/discord/client.js";
-import Websocket from "./structure/websocket/Websocket.js";
-import * as Config from "./config.js";
+import Bot            from "./structure/mineflayer/Bot.js";
+import Client         from "./structure/discord/client.js";
+import Websocket      from "./structure/websocket/Websocket.js";
+import * as Config    from "./config.js";
 export * as endpoints from "./structure/endpoints/endpoints.js";
-export * as logger from "./functions/utils/logger.js"
+export * as logger    from "./functions/utils/logger.js"
 
 /**
  * Main config and colors .json files.
@@ -13,23 +13,17 @@ export const { colors, config } = Config;
 /**
  * @class Options
  * Main options for various functions.
- */
-export const options = new Config.Options();
-
-/**
+ * 
  * @class Client
  * Discord bot class
- */
-export const client = new Client(options.discord);
-
-/**
+ * 
  * @class Bot
  * Mineflayer bot class
- */
-export const bot = new Bot(options.mineflayer);
-
-/**
+ * 
  * @class Websocket
  * Main websocket class for mineflayer bot.
  */
+export const options   = new Config.Options();
+export const client    = new Client(options.discord);
+export const bot       = new Bot(options.mineflayer);
 export const websocket = new Websocket(options.websocketUrl);
