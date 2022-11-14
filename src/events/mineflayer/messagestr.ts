c876e867616ed8f138625daefe2686ed91acc783
Febzey <braydent1235@gmail.com>
2022-11-13 19:23:42 -0600

    sloppy try catch if username isnt there

--- src/events/mineflayer/messagestr.ts
+++ src/events/mineflayer/messagestr.ts
@@ -12,38 +12,39 @@ export default {
     run: async (args: any[], Bot: Bot) => {
         const message = args[0];
         const words = message.split(" ");
-
-        if (
-            message.includes("has made the advancement") ||
-            message.includes("has completed the challenge")
-        ) {
-            client.chatEmbed(`> ${message}`, "yellow");
-            return;
-        }
-
-        if (
-            ((args[2] && args[2]["json"].translate) && args[2]["json"].translate.includes("death")) ||
-            (words[0] === Bot.bot.players[words[0]].username && !dividers.some((divider) => message.includes(divider)))
-        ) {
-
-            const victim = words[0];
-            let murderer = null;
-
-            for (const word of words) {
-                if (Bot.bot.players[word] && word !== victim) {
-                    murderer = word;
-                    break;
-                }
+        try {
+            if (
+                message.includes("has made the advancement") ||
+                message.includes("has completed the challenge")
+            ) {
+                client.chatEmbed(`> ${message}`, "yellow");
+                return;
             }
 
-            await client.chatEmbed(`> ${message}`, "purple").catch(() => { });
+            if (
+                ((args[2] && args[2]["json"].translate) && args[2]["json"].translate.includes("death")) ||
+                (words[0] === Bot.bot.players[words[0]].username && !dividers.some((divider) => message.includes(divider)))
+            ) {
 
-            murderer
-                ? Bot.endpoints.savePvpKill(victim, murderer, message, Bot.mc_server)
-                : Bot.endpoints.savePveKill(victim, message, Bot.mc_server)
+                const victim = words[0];
+                let murderer = null;
 
-        }
+                for (const word of words) {
+                    if (Bot.bot.players[word] && word !== victim) {
+                        murderer = word;
+                        break;
+                    }
+                }
 
-        return;
+                await client.chatEmbed(`> ${message}`, "purple").catch(() => { });
+
+                murderer
+                    ? Bot.endpoints.savePvpKill(victim, murderer, message, Bot.mc_server)
+                    : Bot.endpoints.savePveKill(victim, message, Bot.mc_server)
+
+            }
+
+            return;
+        } catch { }
     }
 }
