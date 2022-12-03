d85fda102a392b8f4137adf38d333f7e169f2b97
Febzey <braydent1235@gmail.com>
2022-12-02 18:28:32 -0600

    Hopefully fixed the playtime intrvl bug.

--- src/events/mineflayer/login.ts
+++ src/events/mineflayer/login.ts
@@ -1,22 +1,27 @@
 import type Bot   from "../../structure/mineflayer/Bot.js"
 import { logger } from "../../index.js";
 
+let intrvl: NodeJS.Timer;
+
 export default { 
     name: "login",
     once: true,
     run: (args:[], Bot: Bot) => {
         logger.log(`> Connected to ${Bot.options.host} successfully`, "green", true);
 
-        setInterval(async () => {
+        if (intrvl) {
+            clearInterval(intrvl);
+        };
+
+        intrvl = setInterval(async () => {
             await Bot.endpoints.savePlaytime(
                 Object.keys(Bot.bot.players),
                 Bot.mc_server
             );
 
-
-            let playerList = Bot.getPlayers()
-            await Bot.endpoints.updateplayerlist(playerList, Bot.mc_server);
+            await Bot.endpoints.updateplayerlist(Bot.getPlayers(), Bot.mc_server);
 
         }, 60000);
+
     }
 }
