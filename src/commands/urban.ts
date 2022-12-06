308a2b4f1fa30a7b7b0be5ffb786bb00a64becd2
Febzey <braydent1235@gmail.com>
2022-12-05 22:05:32 -0600

    okay

--- src/commands/urban.ts
+++ src/commands/urban.ts
@@ -1,11 +1,18 @@
 import type Bot from '../structure/mineflayer/Bot.js';
 import ud       from 'urban-dictionary';
 
+
+const disallowedDefs = ["mule"]
+
 export default {
     commands: ['urban'],
     minArgs: 0,
     maxArgs: 1,
     execute: async (user: string, args: any[], bot: Bot) => {
+
+        if(disallowedDefs.some(def => args.join(" ").toLowerCase().includes(def))) 
+            return bot.bot.whisper(user, "This definition breaks the bot so therefore is not allowed because febzey is too lazy to create a real fix")
+
         return ud.define(`${args.join(" ")}`, (error:unknown, results:any[]) => {
             if (error) return bot.bot.whisper(user, "No results found.");
             
