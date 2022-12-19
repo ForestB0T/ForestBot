c2c05ed3026682f3348d42265f98b3e8a39c93b0
Febzey <braydent1235@gmail.com>
2022-12-18 22:59:20 -0600

    added !mount command which will mount closest boat

---
+++ src/commands/mount.ts
@@ -0,0 +1,19 @@
+import type Bot from '../structure/mineflayer/Bot.js';
+
+export default {
+    commands: ['mount', 'ride'],
+    minArgs: 0,
+    maxArgs: 0,
+    execute: (user: string, args: any[], bot: Bot) => {
+        const nearestBoat = bot.bot.nearestEntity((entity: any) => entity.displayName === "Boat");
+
+         bot.bot.whisper(user, "Searching for nearest boat...")
+
+        if (nearestBoat) {
+            bot.bot.mount(nearestBoat);
+            return bot.bot.whisper(user, "I mounted the nearest boat!")
+        } else {
+            return bot.bot.whisper(user, "I could not find a boat.")
+        }
+    }
+ } as MCommand
