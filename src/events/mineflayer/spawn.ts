0221e01e71585c644e6f9ababb5ad35b5daf3d3a
Febzey <braydent1235@gmail.com>
2023-03-02 03:13:07 -0600

    w

--- src/events/mineflayer/spawn.ts
+++ src/events/mineflayer/spawn.ts
@@ -1,13 +1,13 @@
 import type Bot   from "../../structure/mineflayer/Bot.js"
 import time       from "../../functions/utils/time.js";
+import { config } from "../../config.js";
 export default { 
     name: "spawn",
     once: true,
     run: async (args: any[], Bot: Bot) => {
         Bot.restartCount = 0;
         Bot.isConnected = true;
-        await time.sleep(2000);
-        await Bot.bot.look(180, 0, true);
+        if (config.rotateHeadOnJoin) await time.sleep(2000);
         return
     }
 }
