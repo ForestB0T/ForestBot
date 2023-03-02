9b55662807e58b87e25182204ac774fd92ea1871
Febzey <braydent1235@gmail.com>
2023-03-02 03:19:14 -0600

    w

--- src/events/mineflayer/chat.ts
+++ src/events/mineflayer/chat.ts
@@ -32,10 +32,11 @@ function antiSpamHandler(args: antiSpamArgsType): boolean {
 }
 
 export default {
-    name: "chat:chat",
+    name: "chat",
     once: false,
     run: async (args: any[], Bot: Bot) => {
         const content: BotEvents = args[0];
+        console.log(args);
         try {
 
             const user = {
