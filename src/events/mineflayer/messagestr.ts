6a32403b995b1b7ce5af4b8d6a855d132d3c6e6c
Febzey <braydent1235@gmail.com>
2023-03-03 01:19:51 -0600

    w

--- src/events/mineflayer/messagestr.ts
+++ src/events/mineflayer/messagestr.ts
@@ -50,6 +50,7 @@ export default {
             if (config.useRawChat && args[1] === "chat") {
                 console.log(args);
                 console.log(args[3])
+                console.log(Bot.bot.players)
                 const username = Bot.bot.players[args[3]].username;
                 await Bot.endpoints.saveChat(
                     username,
