51d0fcedb3fd5b292ef16fddb0ebf19c3d3f6cd3
Febzey <braydent1235@gmail.com>
2023-03-07 03:14:03 -0600

    ww

--- src/commands/lastmessage.ts
+++ src/commands/lastmessage.ts
@@ -12,7 +12,7 @@ export default {
         if (!data) return
 
         const lastMessage = data.messages[0];
-        const lastMsgStr = `"${lastMessage.message}" ${lastMessage.date !== null &&  `(${time.timeAgoStr(parseInt(lastMessage.date))})`}`
+        const lastMsgStr = `"${lastMessage.message}" ${lastMessage.date !== null ? `(${time.timeAgoStr(parseInt(lastMessage.date))})`:""}`
 
         return !args[0]
         ? bot.bot.whisper(user, `${lastMsgStr}`)
