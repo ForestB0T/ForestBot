98fe762d4a4d284aa632d58e7622a15f5fe2fc3f
Febzey <braydent1235@gmail.com>
2023-03-27 08:02:13 -0500

    ww

--- src/structure/mineflayer/utils/unicodeTransformer.ts
+++ src/structure/mineflayer/utils/unicodeTransformer.ts
@@ -4,6 +4,7 @@ import { config } from "../../../config.js";
 const fonts = getFonts();
 
 export default function convertToUnicode(text: string, font?: string) {
+    if (text.includes("§")) text.replaceAll("§", "s");
     const s = transform(text, fonts[`${config.unicodeFont}`])
     return s;
 }   
