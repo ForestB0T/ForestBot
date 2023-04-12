a010d8bfa3b06c74aecfba0981f54ed58db2886e
Febzey <braydent1235@gmail.com>
2023-04-11 20:57:07 -0500

    ww

--- src/functions/chatgpt/ai.ts
+++ src/functions/chatgpt/ai.ts
@@ -32,9 +32,10 @@ export default async function ForestBotAi(username: string, message: string, Bot
             const response = await openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: prompt,
-                temperature: 0.9,
+                temperature: 0.6,
                 max_tokens: 120,
                 user: username,
+                n: 1,
             });
 
             // handle the response here
