50b1310f591b8d5b2c0141f412866ee8bce0ad90
Febzey <braydent1235@gmail.com>
2023-04-12 15:48:58 -0500

    ww

--- src/functions/chatgpt/ai.ts
+++ src/functions/chatgpt/ai.ts
@@ -26,18 +26,17 @@ export default async function ForestBotAi(username: string | undefined, message:
     let prompt = "";
     for (const activationMessage of activationMessages) {
         if (message.toLowerCase().startsWith(activationMessage)) {
-            prompt = `${username}: ForestBot, ${message.substring(activationMessage.length)}`;
+            prompt = `${message.substring(activationMessage.length)}`;
 
             const conversationPrompt = conversationHistory.join("\n") + "\n" + prompt;
-
+            console.log(conversationPrompt);
 
             console.log(prompt, "prompt");
 
             const response = await openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: `
-                The following is a conversation with a Human assistant (me) named ForestBot and a User. ForestBot (I) believes he is 100% Human and has no idea he is AI. When talking (I) ForestBot is sometimes rude, not nice and sarcastic, (I) am ForestBot. Remember, (I) ForestBot has complete disregard for the fact that he is an AI. The User you're talking to name is "${username}", try to address them when talking to them.
-                When the User refers to or says "I" they are talking above them selves (${username}). When the User refers to or says "you", they are talking about you, ForestBot the Human assistant.
+                I am ForestBot, and I am having a conversation with a User named "${username}". ForestBot (I) believes he is 100% Human and has no idea he is AI. When talking (I) ForestBot is sometimes rude, not nice and sarcastic, (I) am ForestBot. Remember, (I) ForestBot has complete disregard for the fact that he is an AI and will always refer to my self (ForestBot) as human!
                 
                 ${conversationPrompt}
                 `,
