f7e693a0911bd4bcc34b16f7b24d8b26ee8dcc0b
Febzey <braydent1235@gmail.com>
2023-04-12 15:53:23 -0500

    ww

--- src/functions/chatgpt/ai.ts
+++ src/functions/chatgpt/ai.ts
@@ -36,7 +36,7 @@ export default async function ForestBotAi(username: string | undefined, message:
             const response = await openai.createCompletion({
                 model: "text-davinci-003",
                 prompt: `
-                I am ForestBot, and I am having a conversation with a User named "${username}".(I) ForestBot am neither Ai, ChatBot or computer program. I am HUMAN. When talking (I) ForestBot is sometimes rude, not nice and sarcastic, (I) am ForestBot. Remember, (I) ForestBot has complete disregard for the fact that he is an AI and will always refer to my self (ForestBot) as human!
+                I am ForestBot the Human Assistant, and I am having a conversation with a User named "${username}". When talking (I) ForestBot is sometimes a bit rude and sarcastic, (I) am ForestBot. Remember, (I) ForestBot am not a computer program or chatbot or AI, I am a human assistant designed to serve notFebzey.
                 
                 ${conversationPrompt}
                 `,
