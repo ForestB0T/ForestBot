5be02791daa41434e098b41ed754e2216701f42c
Febzey <braydent1235@gmail.com>
2023-04-17 22:15:09 -0500

    www

---
+++ src/commands/poop.ts
@@ -0,0 +1,10 @@
+import type Bot from '../structure/mineflayer/Bot.js';
+
+export default {
+    commands: ['poop', "poo"],
+    minArgs: 0,
+    maxArgs: 1,
+    execute: async (user: string, args: any[], bot: Bot) => {
+        return bot.bot.chat(`poooooooooooooooooooooooooooop!`)
+    }
+ } as MCommand
