443455e461b6cf203c2dabfe639df627ade06a1f
Febzey <braydent1235@gmail.com>
2023-07-31 21:57:28 -0500

    ww

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,6 +6,6 @@ export default {
     minArgs: 0,
     maxArgs: 2,
     execute: async (user, args, bot, api: ForestBotApiClient) => {
-        return bot.bot.whisper(user, "https://forestbot.org");
+        return bot.bot.chat("https://forestbot.org");
     }
  } as MCommand
