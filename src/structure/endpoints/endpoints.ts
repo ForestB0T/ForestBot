1bac57d0ab471d1d23f2166037c18b54b6c883a1
Febzey <braydent1235@gmail.com>
2024-12-26 21:25:33 -0600

    enhance websocket reconnection logic

--- src/structure/endpoints/endpoints.ts
+++ src/structure/endpoints/endpoints.ts
@@ -3,6 +3,12 @@ import { Logger, bot } from "../../index.js";
 import { config } from "../../config.js";
 
 import { DiscordChatMessage, ForestBotAPI, ForestBotAPIOptions, NewUserData, NewUserNameData } from "forestbot-api-wrapper-v2";
+import time from "../../functions/utils/time.js";
+
+let reconnectCount = 0;
+
+/* Milliseconds */
+let websocketReconnectCooldown = 5000
 
 export default class apiHandler extends ForestBotAPI {
 
@@ -11,11 +17,28 @@ export default class apiHandler extends ForestBotAPI {
         super(options)
 
         this.on("websocket_open", () => {
+            Logger.websocket(`Websocket connection has been reopened after ${reconnectCount} reconnections!`)
+            reconnectCount = 0;
+
             Logger.websocket("Websocket connection opened")
         });
 
-        this.on("websocket_close", (data: any) => {
+        this.on("websocket_close", async () => {
             Logger.websocket("Websocket connection has been closed!")
+
+            if (reconnectCount >= 5) {
+                Logger.websocket("Websocket connection has been closed 5 times in a row. Will try again in 60 seconds.")
+                websocketReconnectCooldown = 60000;
+            }
+
+            reconnectCount++;
+
+            // recreate the api handler after 5 seconds
+            await time.sleep(websocketReconnectCooldown);
+            const newApiHandler = new apiHandler(options);
+            Object.assign(this, newApiHandler);
+            
+            return;
         })
 
         this.on("websocket_error", (data: any) => {
