4b0478ae191c04fe291855077398be06b2a7f48c
Febzey <braydent1235@gmail.com>
2025-01-18 23:29:24 -0600

    Improve formatting of online users reply message for clarity

--- src/commands/noobs.ts
+++ src/commands/noobs.ts
@@ -12,7 +12,7 @@ export default {
         const onlineUsernames:string[] = Object.values(bot.bot.players).map(player => player.username);
         const users = await api.getUsersSortedByJoindate(bot.mc_server, 3, 'DESC', onlineUsernames);
 
-        const replyMsg = `The 3 newest users online are:\n${users.map((user, index) => {
+        const replyMsg = `The 3 newest users online are: ${users.map((user, index) => {
             const joinDateStr = user.joindate && !isNaN(Number(user.joindate)) ? time.timeAgoStr(Number(user.joindate)) : 'unknown';
             return `${user.username} (${joinDateStr}), `;
         }).join('\n')}`;
