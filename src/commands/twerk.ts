040ee804de45088cc419b3ead36e3f657ffe1780
Febzey <braydent1235@gmail.com>
2025-01-28 09:14:33 -0600

    Add twerk command for bot to perform a dance action

---
+++ src/commands/twerk.ts
@@ -0,0 +1,23 @@
+import type { ForestBotAPI } from "forestbot-api-wrapper-v2";
+import { config } from '../config.js';
+import time from "../functions/utils/time.js";
+import type Bot from "../structure/mineflayer/Bot.js";
+
+export default {
+    commands: ['twerk', 'bootyshake', 'booty', 'dance'],
+    description: `I will twerk for 10 seconds on your command Usage: ${config.prefix}twerk`,
+    minArgs: 0,
+    maxArgs: 1,
+    execute: async (user, args, bot: Bot, api: ForestBotAPI) => {
+        const endTime = Date.now() + 10000;
+        let sneakState = false;
+        const interval = setInterval(() => {
+            sneakState = !sneakState;
+            bot.bot.setControlState('sneak', sneakState);
+            if (Date.now() >= endTime) {
+                clearInterval(interval);
+                bot.bot.setControlState('sneak', false);
+            }
+        }, 500);
+    }
+} as MCommand
