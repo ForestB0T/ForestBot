bba6a742d255ec37d140941c9ba063bf92121642
Febzey <braydent1235@gmail.com>
2025-01-27 21:11:38 -0600

    Add physicsTick event to handle entity interaction

---
+++ src/events/mineflayer/physicsTick.ts
@@ -0,0 +1,16 @@
+
+import type Bot      from '../../structure/mineflayer/Bot.js';
+import { BotEvents } from 'mineflayer';
+import { api, Logger }    from '../../index.js';
+
+export default {
+    name: 'physicsTick',
+    once: true,
+    run: async (args: any[], Bot: Bot) => {        
+        const content: BotEvents = args[0];
+        const closestEntity = Bot.bot.nearestEntity();
+        if (closestEntity) {
+          Bot.bot.lookAt(closestEntity.position.offset(0, 1, 0)); // Offset to look at the player's head
+        }
+    }
+};
